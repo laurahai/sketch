43a784bdcbc3e3dd261ca56eb828cc2708f70643
miseilath <akileilath@outlook.com>
2022-10-11 15:51:25 -0500

    Create grid with javascript and flexbox layout

--- javascript.js
+++ javascript.js
@@ -1,3 +1,18 @@
 
-const grid_container = document.querySelector(".grid-container");
+var gridContainer = document.querySelector('.grid-container');
 
+function createGridBoxes(size) {
+    for (let i=1; i <= size; i++) {
+        let row = document.createElement('div');
+        row.classList.add('rows');
+        gridContainer.appendChild(row);
+
+        for (let j = 1; j <= size; j++) {
+            let column = document.createElement('div');
+            column.classList.add('columns');
+            row.append(column);
+        }
+    }
+}
+
+createGridBoxes(16);
